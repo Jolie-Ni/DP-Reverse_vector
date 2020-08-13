0dbc66271b26a4f4b1eda9e3570b7d3bd8e96645
Nan Ni <ninan980805@gmail.com>
2020-08-13 18:32:36 -0400

    change folder structure

---
+++ cmake-build-debug/CMakeFiles/DP_Reverse_vector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DP_Reverse_vector.dir/main.cpp.o"
+  "DP_Reverse_vector"
+  "DP_Reverse_vector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DP_Reverse_vector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -122,5 +122,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/reverse_vector.dir/DependInfo.cmake"
+  "CMakeFiles/DP_Reverse_vector.dir/DependInfo.cmake"
   )
